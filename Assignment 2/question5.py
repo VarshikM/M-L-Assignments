003d8e513acc6f59f8db6aef51b410cae98f8aae
VarshikM <vxm74870@ucmo.edu>
2024-06-04 18:20:30 -0500

    Update question5.py

--- Assignment 2/question5.py
+++ Assignment 2/question5.py
@@ -1,1 +1,8 @@
 
+def string(s): # using function
+    u = sum(1 for i in s if i.isupper()) # using is upper function and finding total upper letters
+    l = sum(1 for i in s if i.islower()) # using is lower function and finding total lower letters
+    print("No. of Upper case characters:", u)
+    print("No. of lower case characters:", l)
+string('The quick Brow Fox')
+
