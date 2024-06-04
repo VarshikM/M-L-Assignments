6d01303a5a104faa8f4a29305e81e3ecea819b25
VarshikM <vxm74870@ucmo.edu>
2024-06-04 18:10:01 -0500

    Delete Question8.py

--- Question8.py
+++
@@ -1,8 +0,0 @@
-# Assigning the value of radius and calculation of area
-radius = 10
-area = 3.14*radius**2
-
-# Used the string format function to display the output as required
-print("The area of a circle with radius {} is {} meters square".format(radius, area))
-
-
