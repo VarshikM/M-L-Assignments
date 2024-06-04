8ad3dd30524e3cfa9a3dfc58618c4c9ad209778c
VarshikM <vxm74870@ucmo.edu>
2024-06-04 17:52:14 -0500

    Update question2.py

--- Assignment 1/question2.py
+++ Assignment 1/question2.py
@@ -1,1 +1,27 @@
 
+# creating an empty dictionary
+dog = {}
+print(dog)
+# adding to dictionary
+dog = {"name": "brownie", "color": "brown", "breed": "chow-chow", "legs": "4", "age": "3"}
+print("The updated dog dictionary:", dog)
+# creating a student dictionary
+student = {"first_name": "Varshik", "Last_name": "Mankala", "gender": "male", "age": "22", "marital_status": "single", "skills": ["Programming"], "Country": "India", "City": "Tanuku", "address": "ws/dw/12"}
+student1 = student.keys()
+print("The keys of dictionary student is:", student1)
+# Length of student dictionary
+length = len(student)
+print(length)
+# getting the values of a particular key in dictionary
+print(student["skills"])
+# Modifying dictionary
+student["skills"].extend(["Leadership", 'cricket'])
+print(student['skills'])
+# getting dictionary keys
+key_List = list(student.keys())
+print(key_List)
+# getting values as a list
+value_list =list(student.values())
+print(value_list)
+
+
