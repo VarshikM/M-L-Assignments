1481ac875cfb5b6a614ae6f351f2995f8fd676fa
VarshikM <vxm74870@ucmo.edu>
2024-06-04 18:19:56 -0500

    Update question4.py

--- Assignment 2/question4.py
+++ Assignment 2/question4.py
@@ -1,1 +1,7 @@
-
+def sample_list(l):
+  x = [] # creating an empty list
+  for a in l:
+    if a not in x:
+      x.append(a)  # using append function
+  return x
+print("Unique List:", sample_list([1,2,3,3,3,3,4,5]))
